4b7ec0717578df52c711e862bf08d9de42998a3b
ahmeda-blip <ahmed.a@alwagayan.com>
2026-05-21 12:50:22 +0300

    Add main HRSystemApplication class

---
+++ src/main/java/com/darfaris/hr/HRSystemApplication.java
@@ -0,0 +1,13 @@
+package com.darfaris.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.darfaris.hr")
+public class HRSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HRSystemApplication.class, args);
+    }
+}
